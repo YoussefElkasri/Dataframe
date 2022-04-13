43f43f0e9704d7c356a50f221ac2535a66deba25
YoussefElkasri <elkasri.youssef@outlook.fr>
2022-04-13 15:05:37 +0200

    Creation des 2 methodes de dataFrame

---
+++ app/src/main/java/GroupeY/Csvtodata.java
@@ -0,0 +1,10 @@
+package GroupeY;
+
+public class Csvtodata {
+    
+    String data;
+    public Csvtodata(String data){
+        this.data = data;
+    }
+    
+}
---
+++ app/src/main/java/GroupeY/Insertdata.java
@@ -0,0 +1,5 @@
+package GroupeY;
+
+public class Insertdata {
+    
+}
