2a7a83919b3c25a4f26bde7286f3e4988b1afa48
Ran <ranchengkk@gmail.com>
2022-04-24 21:30:09 +0200

    corrige path

--- app/src/test/java/GroupeY/ArithmetiqueTest.java
+++ app/src/test/java/GroupeY/ArithmetiqueTest.java
@@ -8,7 +8,7 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.ArrayList;
-
+import java.io.File;
 
 /**
  * Unit test for simple App.
@@ -22,7 +22,7 @@ public class ArithmetiqueTest
     @Test
     public void sumTest(){
         try{
-            DataFrame d = new DataFrame("/Users/cr/M1/DevOp/Dataframe/annual_precipitation.csv");
+            DataFrame d = new DataFrame("src/test/resources/annual_precipitation.csv");
             float res = d.sum(3);
             assertEquals(res, 23430.5, 0.5);
         }catch (Exception e) {
@@ -33,7 +33,7 @@ public class ArithmetiqueTest
     @Test
     public void moyenneTest(){
         try{
-            DataFrame d = new DataFrame("/Users/cr/M1/DevOp/Dataframe/annual_precipitation.csv");
+            DataFrame d = new DataFrame("src/test/resources/annual_precipitation.csv");
             float res = d.moyenne(3);
             assertEquals(res, 36.6, 0.2);
         }catch (Exception e) {
@@ -44,7 +44,7 @@ public class ArithmetiqueTest
         @Test
     public void minTest(){
         try{
-            DataFrame d = new DataFrame("/Users/cr/M1/DevOp/Dataframe/annual_precipitation.csv");
+            DataFrame d = new DataFrame("src/test/resources/annual_precipitation.csv");
             float res = d.min(3);
             assertEquals(res, 4, 0.1);
         }catch (Exception e) {
@@ -54,7 +54,7 @@ public class ArithmetiqueTest
     @Test
     public void maxTest(){
         try{
-            DataFrame d = new DataFrame("/Users/cr/M1/DevOp/Dataframe/annual_precipitation.csv");
+            DataFrame d = new DataFrame("src/test/resources/annual_precipitation.csv");
             float res = d.max(3);
             assertEquals(res, 112.9, 0.1);
         }catch (Exception e) {
@@ -65,7 +65,7 @@ public class ArithmetiqueTest
     @Test
     public void countTest(){
         try{
-            DataFrame d = new DataFrame("/Users/cr/M1/DevOp/Dataframe/annual_precipitation.csv");
+            DataFrame d = new DataFrame("src/test/resources/annual_precipitation.csv");
             float res = d.count(3);
             assertEquals(res, 640, 0);
         }catch (Exception e) {
